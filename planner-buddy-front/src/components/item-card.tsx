4aa520fc99cc066ee2a44fb42961c3829a110f97
Bixi0 <bbbissio.a@gmail.com>
2024-11-20 17:02:18 +0000

    migrate useCategoryIndexStore

--- planner-buddy-front/src/components/item-card.tsx
+++ planner-buddy-front/src/components/item-card.tsx
@@ -12,8 +12,8 @@ const Item = ({ category, description, imageUrl }: iItem) => {
     <div className={styles.card}>
       <h2 className={styles.itemCategory}>{category}</h2>
       <Image
-        src={imageUrl}
-        alt={description}
+        src = {imageUrl}
+        alt = {description}
         width={150}
         height={100}
         className={styles.itemImg}
